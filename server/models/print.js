8097788ab67c9dd828dba1fd6c9f0a6c52e4001a
Tatiana Stewart <tstwrt7@gmail.com>
2024-03-28 01:38:26 -0400

    Created models for Users and Prints

---
+++ server/models/print.js
@@ -0,0 +1,56 @@
+const { db, Model, DataTypes } = require('../db/connection');
+
+class Print extends Model {};
+
+Print.init({
+    "catalog_number": {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    "artist": {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    "image": {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    "date": {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    "location": {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    "size": {
+        type: DataTypes.ENUM,
+        values: ["11x14", "16x20", "11x14C"],
+        allowNull: false
+    },
+    "instrument": {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    "status": {
+        type: DataTypes.ENUM,
+        values: ["Available", "Sold", "Unavailable"],
+        allowNull: false
+    },
+    "notes": {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    "date_sold": {
+        type: DataTypes.STRING,
+        allowNull: true
+    }
+}, {
+    sequelize: db,
+    modelName: "Print"
+});
+
+
+module.exports = Print;
+
+
---
+++ server/models/user.js
@@ -0,0 +1,30 @@
+const { db, Model, DataTypes} = require('../db/connection');
+
+class User extends Model {};
+
+User.init({
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        validate: {
+            min: 6
+        }
+    }
+}, {
+    sequelize: db,
+    modelName: "User"
+});
+
+
+module.exports = User;
