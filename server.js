a73c079980f4ac385a41638569305c89dd2b7933
Tatiana Stewart <tstwrt7@gmail.com>
2024-03-29 00:58:33 -0400

    Set up React

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const { db } = require('./server/db/connection');
 const app = require('./server/src/app');
-const port = 5000
-async function init () {
-    app.listen(port, () => {
-        db.sync();
-        console.log(`Listening at http://localhost:${port}`)
-    })    
+const port = 5000;
+
+async function init() {
+    try {
+        await db.sync();
+        app.listen(port, () => {
+            console.log(`Server is running on http://localhost:${port}`);
+        });
+    } catch (error) {
+        console.error('Error syncing database:', error);
+    }
 }
 
-init()
+init();
