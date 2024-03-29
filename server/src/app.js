4df7b57838f35509de9a9d69d713c983951ca623
Tatiana Stewart <tstwrt7@gmail.com>
2024-03-29 00:59:40 -0400

    Added CORS to use API

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
+
 const printsRouter = require('../routes/prints');
 const authRouter = require('../routes/auth');
 const searchRouter = require('../routes/search');
 
-
+app.use(cors());
 app.use(express.json()); // For parsing application/json
 app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
 
