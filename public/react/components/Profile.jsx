e007fa725e38a95f0cf7d342287c1fc94adc4c30
Tatiana Stewart <tstwrt7@gmail.com>
2024-04-17 02:55:10 -0400

    Styled profile

--- public/react/components/Profile.jsx
+++ public/react/components/Profile.jsx
@@ -20,16 +20,16 @@ function Profile({ profileView, setProfileView, userData, email, password, fetch
                     
                     <div id="user-data-div">
                         <div id="user-profile-data">
-                            <h5>Full Name</h5>
+                            <h6>Full Name</h6>
                             <p>{userData && userData.first_name} {userData && userData.last_name}</p>
                             
-                            <h5>Email</h5>
+                            <h6>Email</h6>
                             <p>{userData && userData.email}</p>
                         </div>
 
                         <div id="password-div">
-                            <h5>Password</h5>
-                            <button id="change-pw" className="btn btn-light" onClick={() => handlePWClick()}>Change Password</button>
+                            <h6>Password</h6>
+                            <button id="change-pw" className="btn btn-outline-light" onClick={() => handlePWClick()}>Change Password</button>
                         </div>
                     </div>
                 </>
@@ -42,20 +42,24 @@ function Profile({ profileView, setProfileView, userData, email, password, fetch
 
                 <Modal.Body>
                     <form id="updatePW">
-                        <label htmlFor="current-password">Current Password</label><br/>
-                        <input type="password" name="current-password"></input><br/>
+                        <div>
+                            <label htmlFor="current-password">Current Password</label><br/>
+                            <input type="password" name="current-password"></input><br/>
+                        </div>
 
-                        <label htmlFor="new-password">New Password</label><br/>
-                        <input type="password" name="new-password"></input><br/>
+                        <div>
+                            <label htmlFor="new-password">New Password</label><br/>
+                            <input type="password" name="new-password"></input><br/>
 
-                        <label htmlFor="confirmed-password">Confirm Password</label><br/>
-                        <input type="password" name="confirmed-password"></input>
+                            <label htmlFor="confirmed-password">Confirm Password</label><br/>
+                            <input type="password" name="confirmed-password"></input>
+                        </div>
                     </form>
                     </Modal.Body>
 
                     <Modal.Footer>
                         <button type="button" className="btn btn-outline-danger" onClick={handleCloseModal} >Cancel</button>
-                        <button type="button" className="btn btn-light">Submit</button>
+                        <button type="button" className="btn btn-dark">Submit</button>
                     </Modal.Footer>
             </Modal>
         </>
